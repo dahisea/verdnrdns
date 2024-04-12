acfd371ed994cb5cf9ada88d8f7bc0d3b6fe6854
Dahi <81472122+dahisea@users.noreply.github.com>
2024-04-12 21:42:24 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // 导入必要的模块
-const http = require('http');
+const https = require('https');
 
 // Vercel 函数处理程序
 module.exports = async (req, res) => {
@@ -10,23 +10,26 @@ module.exports = async (req, res) => {
     // 提取原始请求的相关信息
     const { method, url, headers } = req;
 
+    // 解析目标服务器的 URL
+    const targetUrl = 'https://162.14.21.56/dns-query';
+    const { hostname, pathname } = new URL(targetUrl);
+
     // 设置向目标服务器发出请求的选项
     const options = {
       method,
+      hostname,
+      path: pathname,
       headers: {
         ...headers,
         'X-Forwarded-For': clientSubnetIP, // 将 X-Forwarded-For 头部设置为客户端子网 IP 地址
       },
     };
 
-    // 目标服务器的 URL
-    const targetUrl = 'https://162.14.21.56/dns-query';
-
     // 发出代理请求
-    const proxyReq = http.request(targetUrl, options, (proxyRes) => {
+    const proxyReq = https.request(options, (proxyRes) => {
       // 将目标服务器的响应转发回客户端
       res.writeHead(proxyRes.statusCode, proxyRes.headers);
-      proxyRes.pipe(res, { end: true });
+      proxyRes.pipe(res); // 将代理服务器的响应数据流传递回客户端
     });
 
     // 捕获代理请求错误
@@ -36,7 +39,12 @@ module.exports = async (req, res) => {
     });
 
     // 将原始请求正文传递给代理请求（如果有的话）
-    req.pipe(proxyReq, { end: true });
+    req.pipe(proxyReq);
+
+    // 结束响应
+    req.on('end', () => {
+      proxyReq.end();
+    });
   } catch (error) {
     console.error('Function error:', error);
     res.status(500).send('Function execution failed');
